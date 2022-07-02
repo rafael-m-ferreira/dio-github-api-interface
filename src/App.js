a5c2515a50a9a1c70f407f1aeebaa53643896e8c
Rafael M. Ferreira <rafaelmf7@gmail.com>
2022-07-02 14:06:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Layout from './components/layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Layout>
+        <div>
+          <img
+            src="https://avatars.githubusercontent.com/u/67316263?v=4"
+            alt="Avatar of User"
+          />
+          <h1>Rafael Ferreira</h1>
+          <h3>Username: </h3>
+          <span>rafaelmferreira</span>
+          <div>
+            <h4>Folowers</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Folowings</h4>
+            <span>5</span>
+          </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
   );
 }
 
